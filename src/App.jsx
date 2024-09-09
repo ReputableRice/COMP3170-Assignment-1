27e1832b12bb4dcd97497d357608b18db75d23e7
Skailodon <jackiehtruong@outlook.com>
2024-09-09 13:48:34 -0700

    Base Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 function App() {
 
   return (
-    <>
-     <body>
+    <body>
       <h1>MyTunes</h1>
       <div className="artist-profile">
-      <img src="" alt=""/>
-      <div className="discography">
-        <h2>Dareharu</h2>
-        <details name="Dareharu">
-          <p>Karma</p>
-          <p>Flowering</p>
-          <p>Pure Blood</p>
-        </details>
+        <img src="https://lastfm.freetls.fastly.net/i/u/ar0/d677a32dbd232fdc6b579765ec5a5102.jpg" alt="a cartoon girl with a exhausted face" />
+        <div className="discography">
+          <h2>Dareharu Albums</h2>
+          <details name="Dareharu" open="false">
+            <summary>Dareharu</summary>
+            <ol>
+              <li>Karma</li>
+              <li>Flowering</li>
+              <li>Pure Blood</li>
+            </ol>
+          </details>
+        </div>
       </div>
-      </div>
-     </body>
-    </>
+    </body>
   )
 }
 
