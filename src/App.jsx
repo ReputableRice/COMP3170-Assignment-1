0168a09c0096a9d5d0bc8d277cefcd46d595de50
Skailodon <jackiehtruong@outlook.com>
2024-09-09 13:09:18 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+
+  return (
+    <>
+     <body>
+      <h1>MyTunes</h1>
+      <div className="artist-profile">
+      <img src="" alt=""/>
+      <div className="discography">
+        <h2>Dareharu</h2>
+        <details name="Dareharu">
+          <p>Karma</p>
+          <p>Flowering</p>
+          <p>Pure Blood</p>
+        </details>
+      </div>
+      </div>
+     </body>
+    </>
+  )
+}
+
+export default App
